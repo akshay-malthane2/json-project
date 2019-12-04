3ff01e598f974092cb995de9843efb52b25ba5c9
Akshay Malthane <>
2019-12-05 00:16:03 +0530

    Adding Project Files

---
+++ src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { HttpErrorResponse } from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css',
+  '../../node_modules/bootstrap/dist/css/bootstrap.css',
+    '../assets/plugins/fontawesome-free/css/all.min.css',
+    '../assets/dist/css/adminlte.min.css']
+})
+export class AppComponent {
+  title = 'JSON to Table Example';
+  constructor (private httpService: HttpClient) { }
+  arrBirds: string [];
+
+  ngOnInit () {
+    this.httpService.get('./assets/data.json').subscribe(
+      data => {
+        this.arrBirds = data as string [];	 // FILL THE ARRAY WITH DATA.
+        //  console.log(this.arrBirds[1]);
+      },
+      (err: HttpErrorResponse) => {
+        console.log (err.message);
+      }
+    ); 
+  }
+}
